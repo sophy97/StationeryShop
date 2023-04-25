4327e215bdcad49d29dc3446d47222e2f49c393a
sophy97 <naeil_do@naver.com>
2023-04-25 19:20:51 +0900

    1차 완성

--- src/components/Products/ProductIntro.js
+++ src/components/Products/ProductIntro.js
@@ -1,12 +1,21 @@
 /** 상품 출력 페이지 상단, 간단한 쇼핑몰 소개 */
 import React from "react";
 import classes from "./ProductIntro.module.css";
+import { useNavigate } from "react-router-dom";
 
 const ProductIntro = () => {
+  const navigator = useNavigate();
   return (
     <>
       <section className={classes.intro}>
-        <h3>이번 주 인기 상품</h3>
+        <h2>이번 주 인기 상품</h2>
+        <span
+          onClick={() => {
+            navigator("/shop");
+          }}
+        >
+          전체 상품 보기
+        </span>
         <hr />
         <br />
         다양한 문구 용품을 경제적인 가격에 쇼핑하세요!
